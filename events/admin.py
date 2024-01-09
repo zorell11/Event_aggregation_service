7f50dbc6880c15bf627f98b36763bb4d5cdb38a3
Roesova <jakkkdy@seznam.cz>
2024-01-09 21:13:04 +0100

    added upgrade

--- events/admin.py
+++ events/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
-from .models import Category, Event, Comment
+from .models import *
 
 # Register your models here.
 
 admin.site.register(Category)
 admin.site.register(Event)
 admin.site.register(Comment)
+admin.site.register(SigningUp)
