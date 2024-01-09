10d857da34048e2f5dcdfb9393fcac7b81437b44
zorell11 <zorell@azet.sk>
2024-01-09 20:33:20 +0100

    added Comment model

--- events/admin.py
+++ events/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from .models import Category, Event
+from .models import Category, Event, Comment
 
 # Register your models here.
 
 admin.site.register(Category)
 admin.site.register(Event)
+admin.site.register(Comment)
