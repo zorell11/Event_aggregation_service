115ec7efbed6275bc696911c61e6f75ed9edc173
Roesova <jakkkdy@seznam.cz>
2024-01-10 19:54:50 +0100

    added change base and urls

--- event_portal/settings.py
+++ event_portal/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -127,3 +127,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 AUTH_USER_MODEL = 'accounts.CustomUser'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "")
+MEDIA_URL = "images/"
