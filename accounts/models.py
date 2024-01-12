304199dcd3152f542930e2c6a0ce3c886c255469
zorell11 <zorell@azet.sk>
2024-01-12 21:41:08 +0100

    modified accunts model

--- accounts/models.py
+++ accounts/models.py
@@ -4,10 +4,15 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 class CustomUser(AbstractUser):
+    username = models.EmailField(null=False, blank=False)
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=150, blank=False, null=False)
+    last_name = models.CharField(max_length=150, blank=False, null=False)
     phone = models.CharField(max_length=12, null=False, blank=False)
     address = models.TextField(null=False, blank=False)
 
-
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
 
 class Organizer(CustomUser):
     organzier = models.BooleanField(default=True)
