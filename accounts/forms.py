f1a0c8ef3b5c09a3938b840d96098be6cf1851d4
zorell11 <zorell@azet.sk>
2024-01-15 23:57:43 +0100

    added login, logout and sign up

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,1 +1,20 @@
 from django import forms
+
+from django.contrib.auth.forms import UserCreationForm
+
+from .models import CustomUser, Organizer
+
+class UserSignUpForm(UserCreationForm):
+
+    class Meta:
+        model = CustomUser
+        fields = ['email', 'first_name', 'last_name', 'password1', 'password2', 'phone', 'address']
+
+
+
+class OrganizerSignUpForm(UserCreationForm):
+    class Meta:
+        model = Organizer
+        fields = ['email', 'first_name', 'last_name', 'password1', 'password2', 'phone', 'address', 'company_name', 'bank_accunt', 'company_ico']
+
+
