1877bfcacdbf92752ecb2baba220fe43a3ac77c6
zorell11 <zorell@azet.sk>
2024-02-12 20:17:24 +0100

    modified allowed hosts

--- event_portal/settings.py
+++ event_portal/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-061yf28z@f#uga-t*b$_+r-sj^@$!zn_d5)gyvl39-vl1qlqei
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
